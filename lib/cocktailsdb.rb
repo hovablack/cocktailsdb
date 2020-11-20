d14037a56cfd6f827e24eb787b751f16e04e340c
hovablack <nevermind92@hotmail.com>
2020-11-20 17:44:15 -0600

    Initial Setup and cli build

---
+++ lib/cocktailsdb.rb
@@ -0,0 +1,6 @@
+require "cocktailsdb/version"
+
+module Cocktailsdb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cocktailsdb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cocktailsdb do
+  it "has a version number" do
+    expect(Cocktailsdb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
